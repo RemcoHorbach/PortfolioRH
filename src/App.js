e6f37e3441902076a34ecd7c604ce1e13b26682c
RemcoHorbach <horbachremco@gmail.com>
2023-04-22 10:59:35 +0200

    added contact and servermailing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './assets/img/logo.png';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Contact } from './components/Contact';
 import'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Contact/>
     </div>
   );
 }
